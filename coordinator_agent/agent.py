2595c8a0846091d69c6a6166b4be8e978440e787
Nilesh Garg <thenileshgarg001@gmail.com>
2025-09-13 12:25:24 -0400

    set up the basic structure for mvp with agent as tools

--- coordinator_agent/agent.py
+++ coordinator_agent/agent.py
@@ -1,9 +1,16 @@
 from google.adk.agents import LlmAgent
+from google.adk.tools.agent_tool import AgentTool
+
+
 from .prompt import RATING_AGENT_INSTRUCTION
 
-root_agent = LlmAgent(
+MODEL = "gemini-2.5-flash"
+
+coordinator_agent = LlmAgent(
     name="rating_agent",
-    model="gemini-2.5-flash",
+    model=MODEL,
     description="An agent that rates the quality and grantability of a patent application.",
     instruction=RATING_AGENT_INSTRUCTION,
 )
+
+root_agent = coordinator_agent
